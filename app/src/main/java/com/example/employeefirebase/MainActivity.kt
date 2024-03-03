bfd3889c0843a0e4947c08699f76be1553f7f6a9
IrinaBelenko <akaiguren@gmail.com>
2024-03-03 14:29:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/employeefirebase/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.employeefirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+class MainActivity : AppCompatActivity(), OnAuthLaunch, onAddClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun launch(intent: Intent) {
+        startActivityForResult(intent, 1)
+    }
+
+    override fun showListFragment() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, EmployeeListFragment())
+            .commit()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val result = task.getResult(ApiException::class.java)
+                val credential =
+                    GoogleAuthProvider.getCredential(result.idToken, null)
+                val auth = FirebaseAuth.getInstance()
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showListFragment()
+                        } else {
+                            Toast.makeText(
+                                this, "Authentication failed", Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            } catch (e: ApiException) {
+                Toast.makeText(
+                    this, "Error:${e.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    override fun onFabClick() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, AddEmployeeFragment())
+            .addToBackStack("employeeFragment")
+            .commit()
+    }
+
+}
+
+interface OnAuthLaunch {
+    fun launch(intent: Intent)
+    fun showListFragment()
+}
+
+interface onAddClickListener{
+    fun onFabClick()
+}
